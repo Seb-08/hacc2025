489665e212fac97fc2634d0b9ac819abaa3f3930
Seb-08 <sebastiantsutsui@gmail.com>
2025-09-22 19:51:10 -1000

    lesson 9/23

--- src/app/SebastianTiffany/sebastian/page.tsx
+++ src/app/SebastianTiffany/sebastian/page.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
 
-export default function HomePage() {
-	return (
-		<main>
-		</main>
-	);
+export default function SebastianPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <Link href="/">
+        <button className="btn btn-success btn-xl px-8 py-6 text-2xl">
+          Home
+        </button>
+      </Link>
+    </main>
+  );
 }
