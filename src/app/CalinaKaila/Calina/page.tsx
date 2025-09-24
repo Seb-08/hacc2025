2519976494fa0be48cedacbe91843def6f2d0492
CalinaRyan <calina.ryan08@gmail.com>
2025-09-23 16:54:54 -1000

    changed my page.tsx

--- src/app/CalinaKaila/Calina/page.tsx
+++ src/app/CalinaKaila/Calina/page.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
 
-export default function HomePage() {
+export default function CalinaPage() {
 	return (
 		<main>
+			<h1>Calina's Page</h1>
+			<div>
+				<Link href="/">
+					<button className="btn btn-outline btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">Home</button>
+				</Link>
+			</div>
 		</main>
 	);
 }
