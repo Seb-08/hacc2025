d08dbf7d227edd20b84189ad1107286be3e67508
Da-Iba <dibanez11173@gmail.com>
2025-09-23 17:11:27 -1000

    09/23 Danica

--- src/app/TonyDanica/danica/page.tsx
+++ src/app/TonyDanica/danica/page.tsx
@@ -2,7 +2,18 @@ import Link from "next/link";
 
 export default function HomePage() {
 	return (
-		<main>
-		</main>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white ">
+			<h1>Danica's page</h1>
+			<button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
+				<Link href="../TonyDanica/tony">
+					link to tony
+				 </Link>
+			</button>	
+			<button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
+				<Link href="/">
+					link to main
+				 </Link>
+			</button>
+    </main>
 	);
 }
