934ac98367c882395891e78ee0538273739333c8
tfdevil1918 <tfdevil1918@gmail.com>
2025-09-23 17:09:47 -1000

    button and daisyui

--- src/app/JathanHenery/jathan/page.tsx
+++ src/app/JathanHenery/jathan/page.tsx
@@ -2,7 +2,13 @@ import Link from "next/link";
 
 export default function HomePage() {
 	return (
-		<main>
+		<main className="min-h-screen flex items-center justify-center p-8">
+			<div className="text-center">
+                <Link href="/">
+					<button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Yardy</button>
+				</Link>
+            </div>
+			 
 		</main>
 	);
 }
