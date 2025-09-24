9e68d80de2ed318979eb2fc1c98b9e516389c794
tonytsou1 <tonytsou612@gmail.com>
2025-09-23 17:09:44 -1000

    tony23

--- src/app/TonyDanica/tony/page.tsx
+++ src/app/TonyDanica/tony/page.tsx
@@ -1,8 +1,21 @@
 import Link from "next/link";
 
+
+
 export default function HomePage() {
 	return (
-		<main>
+		<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+			<div className="text-center">
+				<h1 className="text-2xl font-bold mb-4">Tony's page</h1>
+				<Link href="/" className="btn btn-soft btn-primary">
+
+					Visit The Main Page
+				</Link>
+
+
+			</div>
 		</main>
 	);
 }
+
+
