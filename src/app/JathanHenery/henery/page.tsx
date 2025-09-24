94fd521ba0ed98c8216edeadc8341f902c9ef381
Henry <henrychristley@gmail.com>
2025-09-23 17:09:40 -1000

    please don't mess this up please

--- src/app/JathanHenery/henery/page.tsx
+++ src/app/JathanHenery/henery/page.tsx
@@ -3,6 +3,11 @@ import Link from "next/link";
 export default function HomePage() {
 	return (
 		<main>
+			<button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+					<Link href="/">
+						Home Page Yardy Know
+					</Link>
+			</button>
 		</main>
 	);
 }
